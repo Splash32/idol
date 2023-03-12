7d2b28e9367c92352096cfccc5753992fdfd2bec
Splash32 <astepura231@gmail.com>
2023-03-12 16:31:04 +0200

    second commit, DZ

---
+++ tuj.py
@@ -0,0 +1,17 @@
+class Iterat:
+
+   def __init__(self, data):
+
+       self.data = data
+
+   def __iter__(self):
+
+       for item in self.data:
+
+           yield item
+
+iterable = Iterat([1, 2, 3, 4, 5])
+
+for item in iterable:
+
+   print(item)
